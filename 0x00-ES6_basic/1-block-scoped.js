cdf29d4a4b23a6ed9cdf87592d94ab94bd352faf
YOUNESSE25 <younessehaouas@gmail.com>
2024-06-26 03:50:44 +0100

    ES6_basic

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  /* change vars to const */
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-  /* change vars to const */
+  /* eslint-enable no-unused-vars */
 
   return [task, task2];
 }
