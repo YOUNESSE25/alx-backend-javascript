067238680ace09bfd5198b344199832e2610b60c
YOUNESSE25 <younessehaouas@gmail.com>
2024-08-21 01:59:03 +0100

    node_js_basic

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+/** small HTTP server using Express module */
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+});
+
+module.exports = app;
