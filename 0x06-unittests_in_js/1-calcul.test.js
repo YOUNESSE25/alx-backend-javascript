10c1fc2367ef7f5dbe1371cff1d00bcd65becbc5
YOUNESSE25 <younessehaouas@gmail.com>
2024-08-22 17:15:44 +0100

    unittests

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -5,8 +5,8 @@ const calculateNumber = require("./1-calcul");
 describe("calculateNumber()", function() {
 
     it(`checking if numbers round`, function() {
-      const res = calculateNumber("SUM", 1, 2);
-      assert.strictEqual(res, 3);
+      const res = calculateNumber("SUM", 3, 2);
+      assert.strictEqual(res, 5);
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("SUBTRACT", 1.4, 2.2);
@@ -17,15 +17,16 @@ describe("calculateNumber()", function() {
       assert.strictEqual(res, 2);
     });
     it(`checking if numbers round`, function() {
-      const res = calculateNumber("DIVIDE", 4, 2);
-      assert.strictEqual(res, 2);
-    });
-    it(`checking if numbers round`, function() {
-      const res = calculateNumber("DIVIDE", 1.7, 0);
-      assert.strictEqual(res, "Error");
+      const res = calculateNumber("DIVIDE", 8, 2);
+      assert.strictEqual(res, 4);
     });
     it(`checking if numbers round`, function() {
       const res = calculateNumber("DIVIDE", 1.4, 4.6);
       assert.strictEqual(res, 0.2);
     });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber("DIVIDE", 1.9, 0);
+      assert.strictEqual(res, "Error");
+    });
+
 });
