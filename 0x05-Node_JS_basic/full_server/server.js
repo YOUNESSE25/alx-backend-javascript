f36f9d95c90fa05d37bf183d335db2f1aeab785a
YOUNESSE25 <younessehaouas@gmail.com>
2024-08-22 15:16:41 +0100

    up

---
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -0,0 +1,19 @@
+/** small Express server
+ *  # It should use the routes defined in
+ *  full_server/routes/index.js
+    # It should use the port 1245
+ */
+const express = require('express');
+
+const router = require('./routes/index');
+
+const app = express();
+const port = 1245;
+
+app.use('/', router);
+app.use('/students', router);
+app.use('/students/:major', router);
+
+app.listen(port);
+
+export default app;
